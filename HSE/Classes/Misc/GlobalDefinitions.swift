19de3dc470105386aea8cd8c6add3afb457e0a0c
Sergei Pankov <sergeipankov@Sergeis-MacBook-Air.local>
2022-07-05 05:21:07 +0100

    config

--- HSE/Classes/Misc/GlobalDefinitions.swift
+++ HSE/Classes/Misc/GlobalDefinitions.swift
@@ -17,8 +17,8 @@ final class GlobalDefinitions {
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
 
-    static let appStoreUrl = "" // TODO
-    static let appleAppID = "1586579843"
+    static let appStoreUrl = "https://apps.apple.com/app/hs-e-map-spec-op-exam-prep/id1631792112"
+    static let appleAppID = "1631792112"
 
     static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
     static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
