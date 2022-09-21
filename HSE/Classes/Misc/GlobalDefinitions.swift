217b4e4f06db9c667fd00a2d5a2a44c0be506023
akonst17 <akonst17@gmail.com>
2022-09-21 19:05:29 +0300

    интеграция sdk 2.0; новые урлы; build 6

--- HSE/Classes/Misc/GlobalDefinitions.swift
+++ HSE/Classes/Misc/GlobalDefinitions.swift
@@ -9,18 +9,21 @@ final class GlobalDefinitions {
     static let domainUrl = "https://exams.mastrapi.com"
     static let apiKey = "AekjshrTk0tBWGpd"
 
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "5kz8Bb4ueuM2sqpB"
-
     static let applicationTag = "EXAM_HSE"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "5kz8Bb4ueuM2sqpB"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "GF8iGjfBXicViscc"
 
     static let appStoreUrl = "https://apps.apple.com/app/hs-e-map-spec-op-exam-prep/id1631792112"
     static let appleAppID = "1631792112"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
